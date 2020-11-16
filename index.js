942a3899c3ce3a381501c1915d9317a5f88f2b45
gerritr56 <68347062+gerritr56@users.noreply.github.com>
2020-11-16 13:57:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const age = 18;
+const isFemale = true;
+const driverStatus = 'bob';
+
+if (age >= 18) {
+    console.log('Jouw leeftijd is 18 of ouder, jij mag naar binnen')
+} else {
+    console.log('Jouw leeftijd is onder de 18, jj mag niet naar binnen')
+}
+
+if (isFemale == true) {
+    console.log('Jij bent een vrouw, jij mag naar binnen')
+} else {
+    console.log('Jij bent geen vrouw, jj mag niet naar binnen')
+}
+
+if (driverStatus == 'bob') {
+    console.log('Jij mag rijden, jij bent de BOB')
+} else {
+    console.log('Jij mag niet rijden, vraag de BOB')
+}
